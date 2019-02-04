1c13bb2f828a351dc34535a01045e5efbb34f486
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-04 23:39:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   def self.new_by_filename(filename)
     song=self.new(filename.split(" - ")[1])
     song.name = filename.split(" - ")[1]
-    song.artist_name = filename.split(" - ")[0]
+    song.artist_name(filename.split(" - ")[0])
     song
   end
 
