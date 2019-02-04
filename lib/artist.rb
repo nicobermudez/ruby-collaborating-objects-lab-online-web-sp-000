e80d7d1f5defea0015442d2250891e5631d9a5d4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-04 23:46:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,10 @@
 class Artist
+  @@all = []
   attr_accessor :name, :songs
 
-  @@all = []
-
   def initialize(name)
-    @name=name
-    @songs =[]
+    @name = name
+    @songs = []
   end
 
   def self.all
@@ -17,7 +16,16 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.find {|artist| artist.name == name} || self.new(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
   end
 
   def save
@@ -25,7 +33,6 @@ class Artist
   end
 
   def print_songs
-    @songs.each {|song| puts song.name}
+    songs.each {|song| puts song.name}
   end
-
 end
