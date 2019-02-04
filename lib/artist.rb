e9fc36eed03f29fefd5941f7c69f0212a5c84f70
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-04 23:51:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.find {|artist| artist.name == name}  || self.new(name)
+    self.all.find {|artist| artist.name == name}  || self.new(name).tap {|artist| artist.save}
   end
 
   def save
