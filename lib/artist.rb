b4aafec9bba4e3e80f40eb4d37d4c89c80ea129e
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-04 22:38:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,13 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs, 
 
   def initialize(name)
     @name=name
+    @songs =[]
   end
 
-  def add_song
+  def add_song(song) 
+    
     
   end
 
